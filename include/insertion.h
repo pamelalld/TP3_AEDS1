20e57d8593e6ebaeda5b814f1d5863fc0a96b7f1
Beatriz Queiroz Cruz Silva <beatriz.cruz.silva@ufv.br>
2025-01-20 10:11:27 -0300

    compartimento arrumando

--- include/insertion.h
+++
@@ -1,8 +0,0 @@
-#ifndef INSERTION_H
-#define INSERTION_H
-
-#include "Compartimento.h"
-
-void insertion_sort(lista_rochas *vetor,int n);
-void ordena(int n,lista_rochas *vetor,int *comp,int *trocas);
-#endif
